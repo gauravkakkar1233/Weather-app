3aef9e88e2c3df3ade4b5d5b2249ec9f3f0055c4
Gaurav Kakkar <9a10gauravkakkar@gmail.com>
2026-02-22 00:51:10 +0530

    Initial commit

--- src/Searchbox.jsx
+++ src/Searchbox.jsx
@@ -4,55 +4,63 @@ import TextField from '@mui/material/TextField';
 import "./Searchbox.css"
 import { useState } from 'react';
 
-function Searchbox({updateInfo}) {
+function Searchbox({ updateInfo }) {
+    console.log("API KEY:", import.meta.env.VITE_WEATHER_API_KEY);
     let [city, setCity] = useState("");
-    let [error,setError]=useState(false);   
+    let [error, setError] = useState(false);
 
-    const API_URL = "https://api.openweathermap.org/data/2.5/weather";
-    const API_KEY = "a1fd5fbeacf29342870c2278caa99c35";
+    const API_URL = import.meta.env.VITE_BASE_URL;
+    const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
     let getweatherinfo = async (cityName) => {
-        try{
-        let response = await fetch(`${API_URL}?q=${cityName}&appid=${API_KEY}&units=metric`);
-        let jsonresponse = await response.json();
-        console.log(jsonresponse);
-        let result={
-            city: cityName,
-            temp:jsonresponse.main.temp,
-            tempMin:jsonresponse.main.temp_min,
-            tempMax:jsonresponse.main.temp_max,
-            humidity:jsonresponse.main.humidity,
-            feelsLike:jsonresponse.main.feels_like,
-            weather:jsonresponse.weather[0].description
-        };
-        console.log(result);
-        return result;
-    }
-    catch(err){
-        throw err;
-    }
-    }
+        try {
+            let response = await fetch(
+                `${API_URL}?q=${cityName}&appid=${API_KEY}&units=metric`
+            );
+
+            if (!response.ok) {
+                throw new Error("City not found");
+            }
+
+            let jsonresponse = await response.json();
+
+            let result = {
+                city: cityName,
+                temp: jsonresponse.main.temp,
+                tempMin: jsonresponse.main.temp_min,
+                tempMax: jsonresponse.main.temp_max,
+                humidity: jsonresponse.main.humidity,
+                feelsLike: jsonresponse.main.feels_like,
+                weather: jsonresponse.weather[0].description
+            };
+
+            return result;
+
+        } catch (err) {
+            throw err;
+        }
+    };
 
     let handlechange = (evt) => {
         setCity(evt.target.value);
-    }
+        setError(false);
+    };
 
     let handlesubmit = async (evt) => {
-        try{
         evt.preventDefault();
-        setCity("");
-        let newinfo=await getweatherinfo(city);
-        updateInfo(newinfo);
+
+        try {
+            let newinfo = await getweatherinfo(city);
+            updateInfo(newinfo);
+            setCity("");
         }
-        catch(err)
-        {
+        catch (err) {
             setError(true);
         }
-    }
+    };
 
     return (
         <div className='Searchbox'>
-            {/* <h2>Search for the web</h2> */}
             <form onSubmit={handlesubmit}>
                 <TextField
                     id="city"
@@ -66,10 +74,15 @@ function Searchbox({updateInfo}) {
                 <Button variant="contained" endIcon={<SendIcon />} type='submit'>
                     Search
                 </Button>
-                {error && <p style={{color:"red"}}>No such place exist</p>}
+
+                {error && (
+                    <p style={{ color: "red", marginTop: "10px" }}>
+                        No such place exists!
+                    </p>
+                )}
             </form>
         </div>
-    )
+    );
 }
 
 export default Searchbox;
